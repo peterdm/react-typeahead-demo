3f50c52bd03561d176778a013a0ccf10fa0e9630
Peter Dixon-Moses <peter.dixon-moses@snagajob.com>
2019-03-25 16:24:51 -0400

    Started restyle.  Next need to emulate classnames from eM.

--- app/scripts/components/app-main.js
+++ app/scripts/components/app-main.js
@@ -12,69 +12,53 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBox fetchMovieID={this.fetchMovieID.bind(this)}/>
+        <SearchBox />
         <Card data={this.state}/>
       </div>
     )
   } // END render
 
-  // the api request function
-  fetchApi(url) {
+  showSuggestionStats(suggestion) {
+    console.log(suggestion);
+    this.setState({
+      querie: suggestion.querie,
+      volume: suggestion.volume,
+      quality: suggestion.quality,
+      weight: suggestion.weight
+    });
+  }
 
-    fetch(url).then((res) => res.json()).then((data) => {
-      // update state with API data
-      this.setState({
-        movieID: data.id,
-        original_title: data.original_title,
-        tagline: data.tagline,
-        overview: data.overview,
-        homepage: data.homepage,
-        poster: data.poster_path,
-        production: data.production_companies,
-        production_countries: data.production_countries,
-        genre: data.genres,
-        release: data.release_date,
-        vote: data.vote_average,
-        runtime: data.runtime,
-        revenue: data.revenue,
-        backdrop: data.backdrop_path
-
-      })
-    })
-
-    .catch((err) => console.log('Movie not found!'))
-
-  } // end function
-
-  fetchMovieID(movieID) {
-    let url = `https://api.themoviedb.org/3/movie/${movieID}?&api_key=1ef28d374513cebe254f6e12e1d27da0`
-    this.fetchApi(url)
-  } // end function
-
-  componentDidMount() {    
-    let url = `https://api.themoviedb.org/3/movie/${this.state.movieID}?&api_key=1ef28d374513cebe254f6e12e1d27da0`
-    this.fetchApi(url)
-
+  componentDidMount() {
     // Configure Bloodhound
-    let suggests = new Bloodhound({      
+    let popularQueries = new Bloodhound({
       datumTokenizer: function(datum) { return Bloodhound.tokenizers.whitespace(datum['value']); },
       queryTokenizer: Bloodhound.tokenizers.whitespace,
       remote: {
-        url: 'https://api.themoviedb.org/3/search/movie?query=%QUERY&api_key=1ef28d374513cebe254f6e12e1d27da0',
-        wildcard: '%QUERY',//Added as a fix to latest version of Bloodhound
-        filter: function(movies) {
-          // Map the remote source JSON array to a JavaScript object array
-          return $.map(movies.results, function(movie) {
+        url: 'http://localhost:9200/universal-search-query-suggest/_search/template',
+        prepare: function (query, settings) {
+          settings.type = "POST";
+          settings.contentType = "application/json; charset=UTF-8";
+          settings.data = JSON.stringify({
+            params: {query},
+            id: "query_suggest_template"
+          });
+
+          return settings;
+        },
+        filter : function(popularQueries){
+          return $.map(popularQueries.hits.hits, function(hit){
             return {
-              value: movie.original_title, //This value is used in datumTokenizer to query the tmdb database
-              id: movie.id
+              value: hit._id,
+              id: hit._id,
+              source: hit._source,
+              score: hit._score
             };
           });
         } // end filter
       }
     }); // end new Bloodhound
 
-    suggests.initialize();
+    popularQueries.initialize();
 
     // END Bloodhound
 
@@ -86,11 +70,11 @@ class App extends Component {
     }, 
     {
       displayKey : function(datum) { return datum.value;},
-      name : 'suggests',
-      source: suggests.ttAdapter()      
+      name : 'popularQueries',
+      source: popularQueries.ttAdapter()
     }).
-    on('typeahead:selected', function(obj, datum) {
-      this.fetchMovieID(datum.id)
+    on('typeahead:cursorchanged', function(obj, datum) {
+      this.showSuggestionStats(datum.source)
     }.bind(this));
 
     //----END Typeahead UI config
--- app/scripts/components/search.js
+++ app/scripts/components/search.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-const TMDBLogo = 'https://www.themoviedb.org/assets/1/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg';
 
 class SearchBox extends Component {
 
@@ -13,12 +11,12 @@ class SearchBox extends Component {
         <div className="row">
 
           <div className="col-xs-12 col-sm-6 col-lg-5">
-            <a href="./" title="ReactJS TMDb Movie Search"><img src={TMDBLogo} className="logo" alt="The Movie Database" /></a>
+            <a href="./" title="eMarketer Autosuggest Demo"><img src="images/emarketer.jpg" className="logo" alt="eMarketer" /></a>
           </div>
           <div className="col-xs-12 col-sm-6 col-lg-7">
             <form className="searchbox">
               <label>
-                <input ref="search suggestion" onClick={this.handleChange} className="searchbox__input typeahead form-control" type="text" placeholder="Search Movie Title..." id="q" />
+                <input ref="search suggestion" onClick={this.handleChange} className="searchbox__input typeahead form-control" type="text" placeholder="What can we answer...?" id="q" />
               </label>
               </form>
           </div>
